3375a61f8428bf08cda36e640003c4734ea3db27
Idah Collin <idah_collin@hotmail.com>
2024-03-22 15:55:40 +0100

    Forgot to save about

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -6,6 +6,6 @@ export const AboutContent = () => {
         <div className='about'>
             <div className='about-text'>Happy Easter! This is a playground for easter animations enabling the user to manually choose weather the animations should be shown or not.
 
-                Animations created using LottieFiles, javascript with hooks and CSS. Let's connect! Find me on LinkedIn → <a href="https://www.linkedin.com/in/idah-collin/">Idah Collin</a></div></div>
+                Animations created using LottieFiles, javascript with hooks and CSS. Let's connect! Find me on LinkedIn → <a href="https://www.linkedin.com/in/idah-collin/" class="my-link">Idah Collin</a></div></div>
     )
 }
