ad11f106b269a62e3cbe73182b9917d3484bf74e
Idah Collin <idah_collin@hotmail.com>
2024-03-25 22:07:00 +0100

    Corrected spelling in about section

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -4,7 +4,7 @@ import "./about.css"
 export const AboutContent = () => {
     return (
         <div className='about'>
-            <div className='about-text'>Happy Easter! This is a playground for easter animations enabling the user to manually choose weather the animations should be shown or not.
+            <div className='about-text'>Happy Easter! This is a playground for easter animations enabling the user to manually choose whether the animations should be shown or not.
 
                 Animations created using LottieFiles, javascript with hooks and CSS. Let's connect! Find me on LinkedIn → <a href="https://www.linkedin.com/in/idah-collin/" class="my-link">Idah Collin</a></div></div>
     )
